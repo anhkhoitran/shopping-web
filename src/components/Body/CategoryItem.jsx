73e5b293c708ac070a1d8ffbe6a4da53a224010d
anhkhoitran <anhkhoitran2509@gmail.com>
2022-07-22 14:58:53 +0700

    add responsive

--- src/components/Body/CategoryItem.jsx
+++ src/components/Body/CategoryItem.jsx
@@ -8,7 +8,7 @@ const CategoryItem = (props) => {
         return (
           <Link to={`/category/${item.name.toLowerCase()}`} key={item.id}>
             <div className="mx-4 border-2 px-4 pt-5 bg-white h-[150px] w-[120px] text-center rounded">
-              <img src={item.img} className="h-[56px] w-[42px] mx-auto " />
+              <img src={item.img} alt="" className="h-[56px] w-[42px] mx-auto " />
               <h1 className="">{item.name}</h1>
             </div>
           </Link>
