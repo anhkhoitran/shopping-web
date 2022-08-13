1ae4e7531e2232af8d10ca36c56a349fcdc1e17e
anhkhoitran <anhkhoitran2509@gmail.com>
2022-08-13 21:35:16 +0700

    clean up unused code

--- src/components/store/index.jsx
+++ src/components/store/index.jsx
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./cart-slice";
-import uiSlice from "./ui-slice";
+// import uiSlice from "./ui-slice";
 
 
 
 const store = configureStore({
     reducer:{
         cart: cartSlice.reducer,
-        ui: uiSlice.reducer,
+        // ui: uiSlice.reducer,
     }
 })
 
